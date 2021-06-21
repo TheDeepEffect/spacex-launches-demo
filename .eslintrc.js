c7c7e2e1ab5bb10879393b9e0790631d4b381abd
Deep Manek <deepmanek25@gmail.com>
2021-06-21 22:48:33 +0530

    Basic project setup

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['airbnb/hooks', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'linebreak-style': ['error', 'unix'],
+    'react/forbid-prop-types': 'off',
+  },
+};
